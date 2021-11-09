02eeadfda82856d7fa3e82c0da036e2dde7d3ccb
Oscar Corral Garcia <oscarcorralg@hotmail.com>
2021-11-09 08:50:55 +0100

    ejercicio3

---
+++ Demo-ejercicio3/src/main/java/com/example/Demoejercicio3/DemoEjercicio3Application.java
@@ -0,0 +1,14 @@
+package com.example.Demoejercicio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEjercicio3Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoEjercicio3Application.class, args);
+	}
+
+}
