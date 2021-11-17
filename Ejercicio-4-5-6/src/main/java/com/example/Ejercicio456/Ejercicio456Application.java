9b84fc0d0f42025601b2e6538474152604e55b1e
Oscar Corral Garcia <oscarcorralg@hotmail.com>
2021-11-17 16:25:49 +0100

    configuración inicial

---
+++ Ejercicio-4-5-6/src/main/java/com/example/Ejercicio456/Ejercicio456Application.java
@@ -0,0 +1,14 @@
+package com.example.Ejercicio456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio456Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Ejercicio456Application.class, args);
+	}
+
+}
