789016a964caba3e59842821401891e63e7d00fe
Oscar Corral Garcia <oscarcorralg@hotmail.com>
2021-11-17 17:27:01 +0100

    add LaptopController

--- Ejercicio-4-5-6/src/main/java/com/example/Ejercicio456/Ejercicio456Application.java
+++ Ejercicio-4-5-6/src/main/java/com/example/Ejercicio456/Ejercicio456Application.java
@@ -1,5 +1,7 @@
 package com.example.Ejercicio456;
 
+import com.example.Ejercicio456.Ejercicio2.Laptop;
+import com.example.Ejercicio456.Ejercicio2.LaptopRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,7 +12,13 @@ public class Ejercicio456Application {
 	public static void main(String[] args) {
 
 		ApplicationContext context =  SpringApplication.run(Ejercicio456Application.class, args);
-		
+		LaptopRepository repository = context.getBean(LaptopRepository.class);
+
+		// Creamos un lapto
+
+		Laptop laptop1 = new Laptop(null,"Toshiba","567hx46",4,"Windows 11");
+		repository.save(laptop1);
+		System.out.println(repository.findAll());
 	}
 
 }
