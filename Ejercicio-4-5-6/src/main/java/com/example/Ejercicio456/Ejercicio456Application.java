012ed00bd61d32bafd20762466e82c1af8e43b97
Oscar Corral Garcia <oscarcorralg@hotmail.com>
2021-11-17 16:46:38 +0100

    añadido Hellocontroller class y final ejercicio1

--- Ejercicio-4-5-6/src/main/java/com/example/Ejercicio456/Ejercicio456Application.java
+++ Ejercicio-4-5-6/src/main/java/com/example/Ejercicio456/Ejercicio456Application.java
@@ -2,13 +2,15 @@ package com.example.Ejercicio456;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Ejercicio456Application {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(Ejercicio456Application.class, args);
+		ApplicationContext context =  SpringApplication.run(Ejercicio456Application.class, args);
+		
 	}
 
 }
