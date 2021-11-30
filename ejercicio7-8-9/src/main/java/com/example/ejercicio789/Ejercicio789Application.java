fb78c36e57ee6f704437371adaef57447751a614
oscar <oscar@escritorio.local>
2021-11-30 12:05:42 +0100

    estructura inicial con dependencias spring

---
+++ ejercicio7-8-9/src/main/java/com/example/ejercicio789/Ejercicio789Application.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio789;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio789Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio789Application.class, args);
+	}
+
+}
---
+++ ejercicio7-8-9/src/test/java/com/example/ejercicio789/Ejercicio789ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio789;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio789ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
