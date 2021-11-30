29dde8ff7c2bd8e8f0f24b8fc2b36807108d0225
oscar <oscar@escritorio.local>
2021-11-30 16:38:52 +0100

    añadido metodo post create para guardar laptop

--- ejercicio7-8-9/src/main/java/com/example/ejercicio789/controller/LaptopController.java
+++ ejercicio7-8-9/src/main/java/com/example/ejercicio789/controller/LaptopController.java
@@ -4,8 +4,11 @@ package com.example.ejercicio789.controller;
 import com.example.ejercicio789.entities.Laptop;
 import com.example.ejercicio789.repository.LaptopRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 
 @RestController
@@ -22,5 +25,16 @@ public class LaptopController {
     @GetMapping("/api/laptops")
     public List<Laptop> findAll(){
         return laptopRepository.findAll();
+    }
+    // GUARDAR UN LAPTOP
+
+    /**
+     *  Guarda un laptop en la base de datos por medio de json
+     * @param laptop nos llega un laptop por json
+     * @return nos devuelve el laptop guardado
+     */
+    @PostMapping("/api/laptop")
+    public Laptop create(@RequestBody Laptop laptop){
+        return laptopRepository.save(laptop);
     }
 }
