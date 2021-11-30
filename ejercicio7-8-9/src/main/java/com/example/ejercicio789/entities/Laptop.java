8d8f11fdf3d39bbe4abeba1812c0ee7a315a7068
oscar <oscar@escritorio.local>
2021-11-30 12:34:41 +0100

    añadido la clase Laptop en el paquete entities

--- ejercicio7-8-9/src/main/java/com/example/ejercicio789/entities/Laptop.java
+++ ejercicio7-8-9/src/main/java/com/example/ejercicio789/entities/Laptop.java
@@ -1,2 +1,66 @@
-package com.example.ejercicio789.entities;public class Laptop {
+package com.example.ejercicio789.entities;
+
+public class Laptop {
+
+    // atributos
+
+    private Long id;
+    private String manufacture;
+    private String model;
+    private Double price;
+    private Boolean touch;
+
+    // constructores
+
+    public Laptop() {
+    }
+
+    public Laptop(Long id, String manufacture, String model, Double price, Boolean touch) {
+        this.id = id;
+        this.manufacture = manufacture;
+        this.model = model;
+        this.price = price;
+        this.touch = touch;
+    }
+    // getter and setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Boolean getTouch() {
+        return touch;
+    }
+
+    public void setTouch(Boolean touch) {
+        this.touch = touch;
+    }
 }
